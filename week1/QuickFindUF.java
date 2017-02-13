015a41347f16845363d8acd079cc18cdf2139f16
zhaoyou <zhaoyou.xt@gmail.com>
2017-02-13 23:18:13 +0800

    update quickfindUF

--- week1/QuickFindUF.java
+++ week1/QuickFindUF.java
@@ -1,18 +1,48 @@
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 /**
  * @author zhaoyou
  */
 public class QuickFindUF {
 
-    private int[] result;
+    private int[] id;
 
     public QuickFindUF(int n) {
-        result = new int[n];
+        id = new int[n];
         for(int i = 0; i < n; i++) {
-            result[i] = i;
+            id[i] = i;
         }
     }
 
-    public static void Main(String [] args) {
+    public void union(int p, int q) {
+        int pid = id[p];
+        int qid = id[q];
+        for(int i = 0; i < id.length; i++) {
+            if (id[i] == pid) {
+                id[i] = qid;
+            }
+        }
+    }
 
+    public boolean connected(int p, int q) {
+        return id[p] == id[q];
+    }
+
+    public static void Main(String [] args) {
+        int N = StdIn.readInt();
+        QuickFindUF uf = new QuickFindUF(N);
+        while (!StdIn.isEmpty())
+        {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (!uf.connected(p, q))
+            {
+                uf.union(p, q);
+                StdOut.println(p + " " + q);
+            }
+        }
     }
 }
